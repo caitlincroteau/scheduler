0fee9e8ad31c3198bde8323b0e61d95ab97fe985
Caitlin Croteau <cjcroteau@gmail.com>
2022-07-13 16:53:58 -0700

    remove extra 'selected' styling

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -27,7 +27,7 @@ export default function DayListItem(props) {
   });
 
   return (
-    <li onClick={() => props.setDay(props.name)} selected={props.selected} className={dayClass}>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{spotsRemaining}</h3>
     </li>
