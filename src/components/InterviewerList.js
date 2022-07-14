a3821affa04d880e9f8ddf6920dd524110aa5caa
Caitlin Croteau <cjcroteau@gmail.com>
2022-07-13 23:06:15 -0700

    add Appointment component, story, directory

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,8 +10,9 @@ export default function InterviewerList(props) {
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        selected={interviewer.id === value}
-        setInterviewer={() => onChange(interviewer.id)}
+        // selected={interviewer.id === value}
+        // setInterviewer={() => onChange(interviewer.id)}
+        // have commented these so story will load correctly
       />
     );
   });
