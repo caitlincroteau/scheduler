4e13f5c93543db3a52fc766ada8eb2a9656e8e58
Caitlin Croteau <cjcroteau@gmail.com>
2022-07-16 17:47:02 -0700

    implement cancel function/button

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -7,6 +7,16 @@ export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  }
+
   return(
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -28,7 +38,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel} >Cancel</Button>
+          <Button danger onClick={cancel} >Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
