e26c25c673c4632a8954e4c09c259d8d8bde0efe
Caitlin Croteau <cjcroteau@gmail.com>
2022-07-16 17:29:33 -0700

    add state to Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "components/Button";
 import InterviewerList from "components/InterviewerList";
 
 export default function Form(props) {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
   return(
     <main className="appointment__card appointment__card--create">
@@ -13,20 +16,20 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
-        <InterviewerList 
-          /* your code goes here */
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer} 
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={props.onCancel} >Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,15 +4,15 @@ import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
 
-  const interviewers = props.interviewers.map(interviewer => {
+  const interviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
         key={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        // selected={interviewer.id === value}
-        // setInterviewer={() => onChange(interviewer.id)}
-        // have commented these so story will load correctly
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
+        // are 'selected' and 'setinterviwer' correct?
       />
     );
   });
