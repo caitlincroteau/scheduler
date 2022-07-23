43fc409273f3f53ebec1af0e1c48d76ac8f36a6b
Caitlin Croteau <cjcroteau@gmail.com>
2022-07-22 23:48:58 -0700

    update conditional rendering for Empty, Show, Form components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,31 +3,47 @@ import 'components/Appointment/styles.scss';
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
 
 export default function Appointment(props) {
 
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE"
 
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+  
   return (
     <article className="appointment">
       <Header
         time={props.time}
       />
       <Fragment>
-        {props.interview ?
-        <>
+        {mode === EMPTY && (
+          <Empty
+            onAdd={() => {transition(CREATE)}}
+          />)}
+
+        {mode === SHOW && (
           <Show
             student={props.interview.student}
-            interviewer={props.interview.interviewer}
+            interviewer={props.interview.interviewer}  
           />
-        </>
-        :
-        <>
-          <Empty />
-        </>
-        }
-      </Fragment>
-      
+        )}
 
+        {mode === CREATE && (
+          <Form
+            interviewers = {[]}
+            onSave={() => console.log("onSave")}
+            onCancel={() => {back()}}
+          />
+        )}
+      </Fragment>  
     </article>
   );
 }
+
