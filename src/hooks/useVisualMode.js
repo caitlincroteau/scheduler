7e41df82f253f8de78a227111883342401294ce1
Caitlin Croteau <cjcroteau@gmail.com>
2022-07-22 17:50:42 -0700

    add back function, update transition function's parameters

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,14 +2,33 @@ import React, { useState } from "react";
 
 
 export default function useVisualMode(initial) {
-
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  function transition(mode) {
+  //history = [initial, mode1]
+
+  function transition(mode, replace = false) {
+    //if replace is true, remove current mode from history
+    //this will then set the current mode in history with the new mode
+    //eg if you click back from THIRD you will go to FIRST not SECOND in history
+    if (replace) {
+      history.pop();
+    }
+    //set new mode and add it to history
     setMode(mode);
+    history.push(mode);
   }
 
-  return { mode, transition };
+  function back() {
+    if (history.length > 1) {
+      //removes the last item in the array - the current mode
+      history.pop();
+      //sets mode to be the prev mode
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
 
 }
 
