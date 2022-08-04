53ba69e1ffb7b70602d9384435743bba5682accd
Caitlin Croteau <cjcroteau@gmail.com>
2022-08-04 16:31:10 -0700

    install prop-types library, add test to InterviewerList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'components/InterviewerList.scss';
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types'; 
 
 export default function InterviewerList(props) {
 
@@ -28,3 +29,7 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
