9bac10a8d5a0a23ca1c80fba3c8584635c868204
Caitlin Croteau <cjcroteau@gmail.com>
2022-08-04 22:33:30 -0700

    replace save function with validate function in Form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -9,6 +9,7 @@ export default function Form(props) {
   //state
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   console.log('interviewer on edit', interviewer)
 
@@ -24,11 +25,24 @@ export default function Form(props) {
     onCancel();
   }
 
-  //for save button
-  const save = function () {
-    //creates new interview object and books the interview (index.js)
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
     onSave(student, interviewer);
   }
+
+  // //for save button
+  // const save = function () {
+  //   //creates new interview object and books the interview (index.js)
+  //   onSave(student, interviewer);
+  // }
 
   return(
     <main className="appointment__card appointment__card--create">
@@ -45,6 +59,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={interviewers}
           value={interviewer}
@@ -54,7 +69,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
