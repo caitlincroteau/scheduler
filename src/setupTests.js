f3fb69aff9fffdb9e9f6d520b2c6336eee48e8de
Caitlin Croteau <cjcroteau@gmail.com>
2022-08-06 12:48:31 -0700

    configure setupTests.js with axios.defaults.baseURL

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,5 @@
 import "@testing-library/react/cleanup-after-each";
 import "@testing-library/jest-dom/extend-expect";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8001";
+// from here: https://stackoverflow.com/questions/59188564/jest-test-passed-but-get-error-connect-econnrefused-127-0-0-180-at-the-end
