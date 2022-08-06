9346be921bed38010ccb7f516d5f8e5c04ea98a7
Caitlin Croteau <cjcroteau@gmail.com>
2022-08-06 15:39:12 -0700

    add notes to Application test file

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -22,29 +22,29 @@ describe("Application", () => {
   
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container, debug } = render(<Application />);
-
+    //wait till data loads before setting appointment variable
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
     const appointment = appointments[0];
-
+    //test add button
     fireEvent.click(getByAltText(appointment, "Add"));
-   
+    //test student name input
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
-
+    //test interview selector
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
+    //test save button
     fireEvent.click(getByText(appointment, "Save"));
-    
+    //expect SAVING mode to show
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
-
+    //wait for mode to change to SHOW, then check that student name input was sent via put request
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-
+    //grab day node that contains "Monday"
     const dayNodes = getAllByTestId(container, "day");
     const day = dayNodes.find(day => queryByText(day, "Monday"))
-
+    //check that Monday node says "not spots remaining"
     expect(getByText(day, "no spots remaining")).toBeInTheDocument(); 
   
   });
