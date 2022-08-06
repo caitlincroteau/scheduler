01823109948aac2920c2932087f187c6a5d4b5ed
Caitlin Croteau <cjcroteau@gmail.com>
2022-08-06 14:04:49 -0700

    ensure mock data and not real data is being used in test files

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,18 +1,47 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
-
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
+// jest.mock("axios");
 import Application from "components/Application";
 
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+  
+    return waitForElement(() => getByText("Monday"))
+      .then(() => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      });
+  });
+  
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    console.log(container);
+
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    console.log(prettyDOM(container));
+
+  
+  });
+})
+
+
+/*
+async/await version:
+
+it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"))
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    });
-});
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});*/
